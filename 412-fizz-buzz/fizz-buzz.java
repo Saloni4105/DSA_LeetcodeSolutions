3a7fb5fd2b1fab0618c1436a9cd83e705b0d704e
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2025-09-07 14:27:03 +0530

    Time: 1 ms (99.74%) | Memory: 45 MB (91.70%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        String nums[] = new String[n];
+        for(int i=1; i<= n; i++)
+        {
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            nums[i-1] = "FizzBuzz";
+        }
+        else if(i % 3 == 0)
+        {
+            nums[i-1] = "Fizz";
+        }
+        else if(i % 5 == 0)
+        {
+            nums[i-1] = "Buzz";
+        }
+        else
+        {
+            nums[i-1] = String.valueOf(i);
+        }
+        }
+        return Arrays.asList(nums);
+    }
+}
