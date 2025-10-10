7eef90aee9b08a732851249426a396fcbd4d4d2e
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2025-10-10 07:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (10.93%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,47 +1,37 @@
 class Solution {
-
-    int t[] = new int[101];
-    int solve(int nums[], int i, int n)
-    {
-        if(i > n)
-        {
-            return 0;
-        }
-
-        // if already computed, return stored value
-        if(t[i] != -1)
-        {
-            return t[i];
-        }
-
-        // Option 1: Rob current house and move to i+2
-        int steal = nums[i] + solve(nums, i+2, n);
-
-        // Option 2: Skip current house and move to i+1
-        int skip = solve(nums, i+1, n);
-
-        return t[i] = Math.max(steal, skip);
-    }
-
     public int rob(int[] nums) {
         int n = nums.length;
+        int t[] = new int[n+1];
 
         if(n == 1)
             return nums[0];
 
-        if(n == 2)
-            return Math.max(nums[0], nums[1]);
+        int result1 = 0;
+        int result2 = 0;
 
-        Arrays.fill(t, -1);
+        //Case-1 (Take from 1st House - Hence skip the last house)
+        t[0] = 0;
+        for(int i=1; i<= n-1; i++)
+        {
+            int skip = t[i-1];
+            int steal = nums[i-1] + (i-2 >= 0 ? t[i-2] : 0);
+            t[i] = Math.max(skip, steal);
+        }
+        result1 = t[n-1];
+       
 
-        // case 1: take 0th index
-        int take0 = solve(nums, 0, n-2);
+        //Case-2 (Take from 2nd House - Hence take the last house)
+        Arrays.fill(t, 0);
+        t[0] = 0;
+        t[1] = 0;
+        for(int i=2; i<=n; i++)
+        {
+            int skip = t[i-1];
+            int steal = nums[i-1] + (i-2 >= 0 ? t[i-2] : 0);
+             t[i] = Math.max(skip, steal);
+        }
+        result2 = t[n];
 
-        Arrays.fill(t, -1);
-
-        // case 2 : take 1st index house
-        int take1 = solve(nums, 1, n-1);
-
-        return Math.max(take0, take1);
+        return Math.max(result1, result2);
     }
 }
