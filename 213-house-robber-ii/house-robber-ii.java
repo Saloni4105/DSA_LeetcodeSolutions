f24eadd247f43b87ecfc0c63d798454e0ba46f81
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2025-10-10 08:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (65.30%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,37 +1,37 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int t[] = new int[n+1];
 
         if(n == 1)
             return nums[0];
 
-        int result1 = 0;
-        int result2 = 0;
+        if(n == 2)
+            return Math.max(nums[0], nums[1]);
 
         //Case-1 (Take from 1st House - Hence skip the last house)
-        t[0] = 0;
-        for(int i=1; i<= n-1; i++)
-        {
-            int skip = t[i-1];
-            int steal = nums[i-1] + (i-2 >= 0 ? t[i-2] : 0);
-            t[i] = Math.max(skip, steal);
-        }
-        result1 = t[n-1];
+        int take_first_home = solve(nums, 0, n-2);
        
 
         //Case-2 (Take from 2nd House - Hence take the last house)
-        Arrays.fill(t, 0);
-        t[0] = 0;
-        t[1] = 0;
-        for(int i=2; i<=n; i++)
-        {
-            int skip = t[i-1];
-            int steal = nums[i-1] + (i-2 >= 0 ? t[i-2] : 0);
-             t[i] = Math.max(skip, steal);
-        }
-        result2 = t[n];
+        int not_take_first_home = solve(nums, 1, n-1);
 
-        return Math.max(result1, result2);
+        return Math.max(take_first_home, not_take_first_home);
+    }
+
+    int solve(int nums[], int l, int r)
+    {
+        int prev =0;
+        int prevPrev = 0;
+
+        for(int i= l; i<=r; i++)
+        {
+            int skip = prev;
+            int take = nums[i] + prevPrev;
+            int temp = Math.max(skip, take);
+
+            prevPrev = prev;
+            prev = temp;
+        }
+        return prev;
     }
 }
