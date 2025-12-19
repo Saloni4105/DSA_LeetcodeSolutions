b2fc48749766e2b365dc10d630ba183237b3f711
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2025-12-19 07:55:12 +0530

    Time: 2 ms (98.73%) | Memory: 44.4 MB (55.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -7,14 +7,17 @@ class Solution {
 
         while(left < right)
         {
+            // ignoring special character
             while(left < right && !Character.isLetterOrDigit(s.charAt(left)))
             {
                 left++;
             }
+            // ignoring special character
             while(left < right && !Character.isLetterOrDigit(s.charAt(right)))
             {
                 right--;
             }
+            // compare by converting in lowercase
             if (Character.toLowerCase(s.charAt(left)) !=
                 Character.toLowerCase(s.charAt(right)))
             {
