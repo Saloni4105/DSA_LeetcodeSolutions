0d17d81d30f698be6f1a75eefe5daa9f09a8c00b
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2026-01-10 14:47:44 +0530

    Time: 268 ms (40.35%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
 
-SELECT MAX(e1.salary) AS SecondHighestSalary
-FROM Employee e1
-WHERE 1 =
-    (
-    SELECT COUNT(DISTINCT e1.salary)
-    FROM Employee e2
-    WHERE e2.salary > e1.salary
-);
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee 
+WHERE salary <(
+    SELECT MAX(salary)
+    FROM Employee 
+)
