0b6f01aeb842c02d21d923a12665374f3160281c
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2026-01-10 14:42:39 +0530

    Time: 652 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
+
 SELECT MAX(e1.salary) AS SecondHighestSalary
 FROM Employee e1
-INNER JOIN Employee e2
-ON e1.salary < e2.salary
+WHERE 1 =
+    (
+    SELECT COUNT(DISTINCT e1.salary)
+    FROM Employee e2
+    WHERE e2.salary > e1.salary
+);
