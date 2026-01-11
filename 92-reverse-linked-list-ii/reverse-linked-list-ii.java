def78ed1035b09b147bd4899e50a253bc28d289c
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2026-01-11 11:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (13.95%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -12,37 +12,37 @@ class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         
         // base case
-        if(head == null || head.next == null)
+        if(head == null || left == right)
         {
             return head;
         }
 
-        // cummy node
-        ListNode temp = new ListNode(0);
-        temp.next = head;
+        // dummy node
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
         // prev - just before left
-        ListNode prev = temp;
+        ListNode prev = dummy;
         for(int i=1; i < left; i++)
         {
             prev = prev.next;
         }
-        //  curr points - first node to reverse part
-        ListNode curr = prev.next;
+        
+        ListNode prevNode = null;
+        ListNode curr= prev.next;
+        ListNode next;
 
-        // reverse node b/w left and right
-        for(int i=1; i <= right - left; i++)
+        for(int i=left; i<=right; i++)
         {
-            // curr first node
-            ListNode Ntemp = prev.next;
-
-            // curr = curr.next
-            prev.next = curr.next;
-
-            curr.next = curr.next.next;
-            prev.next.next = Ntemp;
+            next = curr.next;
+            curr.next = prevNode;
+            prevNode = curr;
+            curr = next;
         }
 
-        return temp.next;
+        prev.next.next = curr; //add with right list
+        prev.next = prevNode;
+
+        return dummy.next;
     }
 }
