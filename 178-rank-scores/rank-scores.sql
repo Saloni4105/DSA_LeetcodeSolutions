4a81d27bf8f62e506e3e8da4756bba488e2c16c6
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2026-01-12 13:08:12 +0530

    Time: 347 ms (26.48%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
-SELECT score, DENSE_RANK() OVER(ORDER BY score DESC) AS `rank` 
-FROM Scores;
+SELECT score, DENSE_RANK() OVER(ORDER BY score DESC) AS `rank`
+FROM Scores
