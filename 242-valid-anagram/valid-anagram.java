51aeba89f8ed1d113100c3161aff8f925935913a
Saloni4105 <142518366+Saloni4105@users.noreply.github.com>
2026-02-01 16:01:37 +0530

    Time: 18 ms (14.63%) | Memory: 46.5 MB (39.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,34 +1,34 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if lengths are not equal, can't be anagram
-        if(s.length() != t.length())
+    //    if length are not equal, not anagram
+    if(s.length() != t.length())
+    {
+        return false;
+    }
+
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    for(int i=0; i<s.length(); i++)
+    {
+        char ch = s.charAt(i);
+        map.put(ch, map.getOrDefault(ch, 0) + 1);
+    }
+
+    // compare
+    for(int i=0; i<t.length(); i++)
+    {
+        char ch = t.charAt(i);
+        if(!map.containsKey(ch))
         {
             return false;
         }
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        // count character of s
-        for(int i=0; i<s.length(); i++)
+        map.put(ch, map.get(ch)-1);
+        if(map.get(ch) == 0)
         {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) +1);
+            map.remove(ch);
         }
-
-        // subtract using character of t
-        for(int i=0; i<t.length(); i++)
-        {
-            char ch = t.charAt(i);
-            if(!map.containsKey(ch))
-            {
-                return false; //charcter not present  
-            }
-            map.put(ch, map.get(ch) -1);
-            if(map.get(ch) == 0)
-            {
-                map.remove(ch);
-            }
-        }
-        // if map is empty, they are anagrams
-        return map.isEmpty();
+    }  
+    return map.isEmpty();
     }
 }
